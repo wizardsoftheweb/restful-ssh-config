6580e09ac38d89da8cdcded27acb2d0c9aa783ae
CJ Harries <cj@wizardsoftheweb.pro>
2018-04-01 11:54:51 -0500

    Update sample launcher

--- sample_launcher.py
+++ sample_launcher.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_pyfile('flask-and-descendants.cfg')
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
