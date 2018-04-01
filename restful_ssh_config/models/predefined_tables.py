1784e39dc783693f54f55221864387325f206bfe
CJ Harries <cj@wizardsoftheweb.pro>
2018-04-01 13:20:39 -0500

    Create lookup tables for advanced linking

---
+++ restful_ssh_config/models/predefined_tables.py
@@ -0,0 +1,38 @@
+"""This file provides predefined lookup tables"""
+
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer as SqlInteger,
+    Table,
+)
+
+KEYWORD_HOST_LOOKUP = Table(
+    Column(
+        'keyword_id',
+        SqlInteger,
+        ForeignKey('keywords.id'),
+        primary_key=True,
+    ),
+    Column(
+        'host_id',
+        SqlInteger,
+        ForeignKey('hosts.id'),
+        primary_key=True,
+    )
+)
+
+HOST_CONFIG_LOOKUP = Table(
+    Column(
+        'host_id',
+        SqlInteger,
+        ForeignKey('hosts.id'),
+        primary_key=True,
+    ),
+    Column(
+        'config_file_id',
+        SqlInteger,
+        ForeignKey('config_files.id'),
+        primary_key=True,
+    )
+)
