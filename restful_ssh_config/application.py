2c43345d2a8e3fb46120c343b29e8cab56be1694
CJ Harries <cj@wizardsoftheweb.pro>
2018-04-01 17:45:04 -0500

    Update application files to use BaseModel

--- restful_ssh_config/application.py
+++ restful_ssh_config/application.py
@@ -2,7 +2,9 @@ from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
+from restful_ssh_config import BaseModel
+
 APPLICATION = Flask(__name__)
 APPLICATION.config.from_pyfile('flask-and-descendants.cfg')
-DATABASE = SQLAlchemy(APPLICATION)
+DATABASE = SQLAlchemy(APPLICATION, model_class=BaseModel)
 MARSHMALLOW = Marshmallow(APPLICATION)
--- sample_launcher.py
+++ sample_launcher.py
@@ -3,6 +3,6 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('flask-and-descendants.cfg')
+app.config.from_pyfile('restful_ssh_config/flask-and-descendants.cfg')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
