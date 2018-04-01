9c16fb7077bb3a89be64c11002d9ab715ce752e0
CJ Harries <cj@wizardsoftheweb.pro>
2018-04-01 13:19:38 -0500

    Draft Host model

---
+++ restful_ssh_config/models/host_model.py
@@ -0,0 +1,50 @@
+"""This file provides the Keyword model"""
+# pylint: disable=too-few-public-methods
+
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Integer,
+    String as SqlString,
+)
+
+from restful_ssh_config import DATABASE
+from restful_ssh_config.models import HOST_CONFIG_LOOKUP, KEYWORD_HOST_LOOKUP
+
+
+class Host(DATABASE.Model):
+    """This class sets up the fields for the Keyword model"""
+
+    __tablename__ = 'hosts'
+
+    id = Column(  # pylint: disable=invalid-name
+        Integer,
+        primary_key=True,
+    )
+    reference_name = Column(
+        SqlString(255)
+    )
+    config_files = DATABASE.relationship(  # pylint: disable=no-member
+        'ConfigFile',
+        secondary=HOST_CONFIG_LOOKUP,
+        lazy='subquery',
+    )
+    keywords = DATABASE.relationship(  # pylint: disable=no-member
+        'Keyword',
+        secondary=KEYWORD_HOST_LOOKUP,
+        lazy='subquery',
+        backref=DATABASE.backref(  # pylint: disable=no-member
+            'hosts',
+            lazy=True
+        ),
+    )
+    created = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
+    updated = Column(
+        DateTime,
+        default=datetime.utcnow
+    )
