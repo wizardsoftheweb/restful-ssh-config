3bbb6b1f3e0f75252dccbd5e7b3833739c83bf05
CJ Harries <cj@wizardsoftheweb.pro>
2018-04-01 16:02:16 -0500

    Load keywords.json and use it validate Keyword

--- restful_ssh_config/schemas.py
+++ restful_ssh_config/schemas.py
@@ -2,6 +2,11 @@
 # pylint: disable=missing-docstring
 # pylint: disable=too-few-public-methods
 
+from json import load as json_load
+from os.path import dirname, join
+
+from marshmallow import validates, ValidationError
+
 from restful_ssh_config import MARSHMALLOW
 from restful_ssh_config.models import (
     ConfigFile,
@@ -9,11 +14,28 @@ from restful_ssh_config.models import (
     Keyword
 )
 
+__location__ = dirname(__file__)
+DATA_DIRECTORY = join(__location__, 'data')
+DATA_FILE = join(DATA_DIRECTORY, 'keywords.json')
+
+KEYWORDS = []
+with open(DATA_FILE, 'r') as keywords_file:
+    KEYWORDS = json_load(keywords_file)
+
 
 class KeywordSchema(MARSHMALLOW.ModelSchema):
 
     class Meta(object):
         model = Keyword
+
+    @validates('keyword')
+    def validates_keyword(self, keyword):  # pylint: disable=no-self-use
+        """Ensures the provided keyword is a valid OpenSSH keyword"""
+        if keyword.lower() not in KEYWORDS:
+            raise ValidationError(
+                "%s is not a current keyword (or the keywords are outdated)"
+                % (keyword)
+            )
 
 
 class HostSchema(MARSHMALLOW.ModelSchema):
