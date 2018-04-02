1075e9c13c17bbb8b6107a4050f07145fe7c0e82
CJ Harries <cj@wizardsoftheweb.pro>
2018-04-01 19:36:35 -0500

    Remove BaseModel from initial call

--- restful_ssh_config/application.py
+++ restful_ssh_config/application.py
@@ -2,9 +2,7 @@ from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
-from restful_ssh_config import BaseModel
-
 APPLICATION = Flask(__name__)
 APPLICATION.config.from_pyfile('flask-and-descendants.cfg')
-DATABASE = SQLAlchemy(APPLICATION, model_class=BaseModel)
+DATABASE = SQLAlchemy(APPLICATION)
 MARSHMALLOW = Marshmallow(APPLICATION)
